19913bf0a133b479512e617670191b40bef90fbc
OshandaB <oshanda893@gmail.com>
2023-04-19 01:25:20 +0530

    supplier Form

--- src/main/java/lk/ijse/pharmacy/controller/SupplierFormController.java
+++ src/main/java/lk/ijse/pharmacy/controller/SupplierFormController.java
@@ -1,4 +1,17 @@
 package lk.ijse.pharmacy.controller;
 
+import javafx.event.ActionEvent;
+
 public class SupplierFormController {
+    public void CustomerAddOnAction(ActionEvent event) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent event) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent event) {
+    }
+
+    public void searchOnAction(ActionEvent event) {
+    }
 }
