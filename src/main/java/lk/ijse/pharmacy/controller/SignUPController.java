6f318198baf2cd785b8d9b0647f63fb5f2d579c9
unknown <valkonx007>
2023-04-19 20:28:32 +0530

    Add Login

---
+++ src/main/java/lk/ijse/pharmacy/controller/SignUPController.java
@@ -0,0 +1,83 @@
+package lk.ijse.pharmacy.controller;
+
+import com.jfoenix.controls.JFXButton;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.TextField;
+import lk.ijse.pharmacy.dto.User;
+import lk.ijse.pharmacy.model.UserModel;
+import lk.ijse.pharmacy.util.Navigation;
+
+public class SignUPController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Hyperlink AlreadyHaveAnAccountHyperLink;
+
+    @FXML
+    private TextField confirmPassword;
+
+    @FXML
+    private TextField emailTxt;
+
+    @FXML
+    private TextField passwordTxt;
+
+    @FXML
+    private JFXButton signUPBtn;
+
+    @FXML
+    private TextField userNameTxt;
+
+    @FXML
+    void hyperLinkOnAction(ActionEvent event) throws IOException {
+        Navigation.switchNavigation("LoginUser.fxml",event);
+
+    }
+
+    @FXML
+    void signBtnOnAction(ActionEvent event) {
+        String username = userNameTxt.getText();
+        String password = passwordTxt.getText();
+        String email = emailTxt.getText();
+
+        User user = new User(username, password, email);
+
+        if (passwordTxt.getText().equals(confirmPassword.getText())) {
+            boolean isSaved = false;
+            try {
+                isSaved = UserModel.save(user);
+                if (isSaved) {
+                    System.out.println("saved");
+                    signUPBtn.getScene().getWindow().hide();
+                    emailTxt.setText("");
+                    userNameTxt.setText("");
+                    passwordTxt.setText("");
+                    confirmPassword.setText("");
+
+                }
+            } catch (Exception throwables) {
+                throwables.printStackTrace();
+            }
+
+        }
+    }
+
+    @FXML
+    void initialize() {
+
+
+    }
+
+}
