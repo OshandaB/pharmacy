7d19c621aaedf57d929bb037160d7df02bbac8c4
maduranga1234 <madurangasadaruwan393@gmail.com>
2023-04-20 00:41:51 +0530

    12.41

--- src/main/java/lk/ijse/pharmacy/model/OrderModel.java
+++ src/main/java/lk/ijse/pharmacy/model/OrderModel.java
@@ -5,6 +5,8 @@ import lk.ijse.pharmacy.util.CrudUtil;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderModel {
     public static int getTotalSales() throws SQLException, ClassNotFoundException {
@@ -15,6 +17,20 @@ public class OrderModel {
             count=resultSet.getInt(1);
         }
         return count;
+    }
+
+    public static List<String> LoadOrderIds() throws SQLException, ClassNotFoundException {
+
+        String sql = "SELECT orderID FROM orders";
+        List<String> allItemData = new ArrayList<>();
+
+        ResultSet resultSet = CrudUtil.crudUtil(sql);
+        while (resultSet.next()) {
+            allItemData.add(resultSet.getString(1));
+
+
+        }
+        return allItemData;
     }
 
 //    public static XYChart.Series lineChartData() throws SQLException, ClassNotFoundException {
